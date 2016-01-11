b5852543289aea89ca626a4657086c0327030227
Victor Häggqvist <victor@snilius.com>
2016-01-11 20:28:48 +0100

    make kudos vanilla es6

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/* eslint-env node */
+
+'use strict';
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var babel = require('gulp-babel');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var size = require('gulp-size');
+
+
+gulp.task('lint', function() {
+    return gulp.src(['src/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
+
+gulp.task('style', function() {
+   return gulp.src('./style/kudos.scss')
+       .pipe(sass({
+           'outputStyle': 'compressed'
+       }))
+       .pipe(size({title: 'css'}))
+       .pipe(rename('kudos.min.css'))
+       .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('style:prefixed', function() {
+    return gulp.src('./style/kudos.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(csso())
+        .pipe(size({title: 'compat css'}))
+        .pipe(rename('kudos.compat.min.css'))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('babel', ['lint'], function () {
+    return gulp.src('./src/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['babel', 'style', 'style:prefixed']);
---
+++ src/Kudoable.js
@@ -0,0 +1,89 @@
+/**
+ * Kudoable
+ *
+ * Based of https://github.com/masukomi/kudos kudos.js
+ *
+ * @author Victor Häggqvist
+ * @since 2016-01-11
+ */
+
+class Kudoable {
+
+    constructor(element) {
+        this.element = document.querySelector(element);
+        this.bindEvents();
+
+        this.counter = this.element.querySelector('.count .num');
+    }
+
+    bindEvents() {
+        this.element.onmouseover = this.start.bind(this);
+        this.element.onmouseout = this.end.bind(this);
+        this.element.onclick = this.unkudo.bind(this);
+        this.element.ontouchstart = this.start.bind(this);
+        this.element.ontouchend = this.end.bind(this);
+    }
+
+    isKudoable() {
+        return /kudoable/.test(this.element.className);
+    }
+
+    isKudod() {
+        return /complete/.test(this.element.className);
+    }
+
+    start() {
+        if (this.isKudoable() && !this.isKudod()) {
+            let event = new Event('kudo:active');
+            this.element.dispatchEvent(event);
+            this.element.classList.add('active');
+            return this.timer = setTimeout(this.complete.bind(this), 700);
+        }
+    }
+
+    complete() {
+        this.end();
+        this.incrementCount();
+        this.element.classList.add('complete');
+
+        let event = new Event('kudo:added');
+        this.element.dispatchEvent(event);
+    }
+
+    end() {
+        if (this.isKudoable() && !this.isKudod()) {
+            this.element.dispatchEvent(new Event('kudo:inactive'));
+            this.element.classList.remove('active');
+
+            if (this.timer != null) {
+                clearTimeout(this.timer);
+            }
+        }
+    }
+
+    unkudo(event) {
+        event.preventDefault();
+        if (this.isKudod()) {
+            this.decrementCount();
+            this.element.classList.remove('complete');
+            this.element.dispatchEvent(new Event('kudo:removed'));
+        }
+    }
+
+    setCount(count) {
+        this.counter.innerHTML = count;
+    }
+
+    currentCount() {
+        return parseInt(this.counter.innerHTML);
+    }
+
+    incrementCount() {
+        this.setCount(this.currentCount() + 1);
+    }
+
+    decrementCount() {
+        this.setCount(this.currentCount() - 1);
+    }
+
+}
